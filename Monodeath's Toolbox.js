13577b143f935fd61ff6685a14430f6807d0e86b
TheRealMonodeath <ryanandmalina4eva@gmail.com>
2024-07-21 08:37:45 -0700

    Update and rename CustomComparatorExtension.js to Monodeath's Toolbox.js

---
+++ Monodeath's Toolbox.js
@@ -0,0 +1,159 @@
+// Name: Monodeath's Toolbox
+// ID: Monodeath
+// Description: Adds various blocks I made :)
+// By: Monodeath <https://scratch.mit.edu/users/BekfastKingRyan/>
+// License: MIT
+
+((Scratch) => {
+    "use strict";
+
+    const cast = Scratch.Cast;
+
+    class ScratchMath {
+        getInfo() {
+            return {
+                id: "Monodeath",
+                name: "Monodeath's Toolbox",
+
+                color1: "#d8a2f2",
+
+                blocks: [{
+                        opcode: "customcomparator",
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: "[A] [B] [C]",
+                        arguments: {
+                            A: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 50,
+                            },
+                            B: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: ">",
+                            },
+                            C: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 50,
+                            },
+                        },
+                    },
+                    {
+                        opcode: "customcomparator2",
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: "[A] [B] [C]",
+                        arguments: {
+                            A: {
+                                type: Scratch.ArgumentType.BOOLEAN,
+                                defaultValue: true,
+                            },
+                            B: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "and",
+                            },
+                            C: {
+                                type: Scratch.ArgumentType.BOOLEAN,
+                                defaultValue: false,
+                            },
+                        },
+                    },
+                    {
+                        opcode: 'setJSONPathToValue',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: 'Set [VALUE] of [PATH] in [JSON_STRING]',
+                        arguments: {
+                            VALUE: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: '5',
+                            },
+                            PATH: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 'fruit/apples',
+                            },
+                            JSON_STRING: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: '{"fruit": {"apples": 2, "bananas": 3}}',
+                            },
+                        },
+                    },
+                ],
+            };
+        }
+
+        customcomparator({
+            A,
+            B,
+            C
+        }) {
+            A = cast.toNumber(A);
+            C = cast.toNumber(C);
+            switch (B) {
+                case ">":
+                    return A > C;
+                case "<":
+                    return A < C;
+                case "=":
+                case "==":
+                    return A == C;
+                case ">=":
+                    return A >= C;
+                case "<=":
+                    return A <= C;
+                case "!=":
+                    return A != C;
+                default:
+                    return false;
+            }
+        }
+
+        customcomparator2({
+            A,
+            B,
+            C
+        }) {
+            A = cast.toBoolean(A);
+            C = cast.toBoolean(C);
+            switch (B) {
+                case "and":
+                    return A && C;
+                case "or":
+                    return A || C;
+                case "xor":
+                    return A !== C;
+                case "nand":
+                    return !(A && C);
+                case "nor":
+                    return !(A || C);
+                case "xnor":
+                    return A === C;
+                default:
+                    return false;
+            }
+        }
+
+        setJSONPathToValue({
+            VALUE,
+            PATH,
+            JSON_STRING
+        }) {
+            try {
+                const path = PATH.toString().split('/').map(decodeURIComponent);
+                let json;
+                try {
+                    json = JSON.parse(JSON_STRING);
+                } catch (e) {
+                    return e.message;
+                }
+                let obj = json;
+                for (let i = 0; i < path.length - 1; i++) {
+                    if (!(path[i] in obj)) obj[path[i]] = {};
+                    obj = obj[path[i]];
+                }
+                obj[path[path.length - 1]] = VALUE;
+                return JSON.stringify(json);
+            } catch (err) {
+                return '';
+            }
+        }
+    }
+
+    Scratch.extensions.register(new ScratchMath());
+})(Scratch);
